adc650181f1604350d72f64109c90128eb0f069d
Sandeep <sandeepwadhawan2001@gmail.com>
2025-07-10 21:42:39 +0530

    added react router dom

--- src/router.ts
+++
@@ -1,9 +0,0 @@
-import { createBrowserRouter } from "react-router-dom";
-import LoginPage from "./pages/LoginPage";
-
-export const router = new createBrowserRouter([
-    {
-        path: "/login",
-        element: <LoginPage />
-    }
-]);
---
+++ src/router.tsx
@@ -0,0 +1,17 @@
+import { createBrowserRouter } from "react-router";
+import LoginPage from "@/pages/Login";
+import HomePage from "@/pages/HomePage";
+
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <HomePage />
+    },
+    {
+        path: "/login",
+        element: <LoginPage />
+    }
+]);
+
+export default router;
