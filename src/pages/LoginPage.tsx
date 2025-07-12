3d9d80a181713f81d340abafffd061d2d0427686
Sandeep <sandeepwadhawan2001@gmail.com>
2025-07-13 02:40:33 +0530

    add tanstack query

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -2,9 +2,22 @@ import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { useRef } from "react";
 import { Link } from "react-router";
 
 const LoginPage = () => {
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+
+  const handleLoginSubmit = async () => {
+    const email = emailRef.current?.value;
+    const password = passwordRef.current?.value;
+    console.log("data", { email, password });
+
+    // Make server call
+    // const data = await axios. 
+  };
+
   return (
     <section className="flex justify-center items-center h-screen">
       <Card className="w-full max-w-sm">
@@ -21,25 +34,25 @@ const LoginPage = () => {
           <div className="grid gap-2">
             <Label htmlFor="email">Email</Label>
             <Input
-              // ref={emailRef}
+              ref={emailRef}
               id="email"
               type="email"
               placeholder="example@example.com"
-              required
+              required={true}
             />
           </div>
           <div className="grid gap-2">
             <Label htmlFor="password">Password</Label>
-            <Input id="password" type="password" placeholder="Enter Password" required />
+            <Input ref={passwordRef} id="password" type="password" placeholder="Enter Password" required />
           </div>
         </CardContent>
         <CardFooter>
           <div className="w-full">
             <Button
-              // onClick={handleLoginSubmit}
+              onClick={handleLoginSubmit}
               className="w-full"
-              // disabled={mutation.isPending}
-              >
+            // disabled={mutation.isPending}
+            >
               {/*  {mutation.isPending && <LoaderCircle className="animate-spin" />} */}
 
               <span className="ml-2">Sign in</span>
