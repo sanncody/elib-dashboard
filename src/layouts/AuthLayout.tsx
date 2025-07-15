eefdbc7ec9fb76fec6b47aefc543f94ae6dc14bc
Sandeep <sandeepwadhawan2001@gmail.com>
2025-07-16 00:13:28 +0530

    make protected routes

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,7 +1,14 @@
-import { Outlet } from "react-router";
+import useTokenStore from "@/store/tokenstore";
+import { Navigate, Outlet } from "react-router";
 
 
 const AuthLayout = () => {
+    const token = useTokenStore(state => state.token);
+
+    if (token) {
+        return <Navigate to={"/dashboard/home"} replace />
+    }
+
     return (
         <>
             <Outlet />
--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -1,13 +1,20 @@
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
-import { Link, Outlet } from "react-router";
+import { Link, Navigate, Outlet } from "react-router";
 import { Bell, CircleUser, Home, LineChart, Menu, Package, Package2, Search, ShoppingCart, Users } from 'lucide-react';
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { Input } from "@/components/ui/input";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import useTokenStore from "@/store/tokenstore";
 
 const DashboardLayout = () => {
+    const token = useTokenStore(state => state.token);
+
+    if (!token) {
+        return <Navigate to={"/auth/login"} replace />
+    }
+
     return (
         <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
             <div className="hidden border-r bg-muted/40 md:block">
