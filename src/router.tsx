26df5cc0a4c2c1b1fc0be0fa20cf90b3354bdb25
Sandeep <sandeepwadhawan2001@gmail.com>
2025-07-23 00:33:26 +0530

    add home page ui

--- src/router.tsx
+++ src/router.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from "react-router";
+import { createBrowserRouter, Navigate } from "react-router";
 import LoginPage from "@/pages/LoginPage";
 import HomePage from "@/pages/HomePage";
 import RegisterPage from "./pages/RegisterPage";
@@ -9,6 +9,10 @@ import CreateBook from "./pages/CreateBook";
 
 
 const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Navigate to={"/dashboard/home"} />
+    },
     {
         path: "dashboard",
         element: <DashboardLayout />,
